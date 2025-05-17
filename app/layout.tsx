1230042cac654f6d3b75ea60edf9aefc5bcd197e
unknown <2020583117@qq.com>
2025-05-17 18:51:50 +0800

    fix: 去除无效的metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,17 +5,14 @@ import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Toaster } from "@/components/ui/toaster"
 
 export const metadata: Metadata = {
   title: {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`
   },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" }
-  ]
+  description: siteConfig.description
 }
 
 interface RootLayoutProps {
@@ -29,6 +26,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Toaster />
           </ThemeProvider>
         </body>
       </html>
