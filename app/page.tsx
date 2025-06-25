256db70bfff2b0125f4662151db61e0911db4f1f
infrost <hx.zeng@hotmail.com>
2025-06-26 03:13:09 +0800

    bug fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,38 @@
+"use client";
+
 import { LinkContent } from "@/components/link-content"
 import { Sidebar } from "@/components/sidebar"
 import { SiteFooter } from "@/components/site-footer"
 import { SiteHeader } from "@/components/site-header"
+import { useState } from 'react';
+import { cn } from "@/lib/utils"
 
 export default function IndexPage() {
+  const [isSidebarOpen, setSidebarOpen] = useState(false);
+
   return (
     <div className="container relative mx-auto min-h-screen w-full px-0">
+      {isSidebarOpen && (
+        <div
+          onClick={() => setSidebarOpen(false)}
+          className="fixed inset-0 z-30 bg-black/50 sm:hidden"
+          aria-hidden="true"
+        />
+      )}
       <div className="flex">
-        <div className="fixed z-20 hidden min-h-screen w-[16rem] transition-all duration-300 ease-in-out sm:block">
-          <Sidebar />
+      {/* 传递 props */}
+        <div
+          className={cn(
+            "fixed z-40 h-screen w-[16rem] bg-background transition-transform duration-300 ease-in-out sm:translate-x-0",
+            isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          )}
+        >
+          {/* Sidebar 组件接收回调 */}
+          <Sidebar onLinkClick={() => setSidebarOpen(false)} />
         </div>
-        <div className="sm:pl-[16rem]">
-          <SiteHeader />
+
+        <div className="w-full sm:pl-[16rem]">
+          <SiteHeader onMenuClick={() => setSidebarOpen(!isSidebarOpen)} />
           <LinkContent />
           <SiteFooter />
         </div>
